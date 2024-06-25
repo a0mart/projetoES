4bb5c35037b529f74389b09f040476ea96c47ae1
Luís Marques <luis2000marques@gmail.com>
2024-06-25 19:35:40 +0100

    update
    adicionei 1 fornecedor ao gestor de base de dados

--- code/src/main/java/MenuPrincipal.java
+++ code/src/main/java/MenuPrincipal.java
@@ -20,6 +20,8 @@ public class MenuPrincipal extends JFrame{
         gestorBaseDados = GestorBaseDados.getGestorBaseDados();
 
         Socio socio3 = new Socio("Manel", 123, "Casa", 345,"Jaaa@");
+        Fornecedor fornecedor1 = new Fornecedor("luis",456,"lisboa",933333333,"email@teste.pt");
+        gestorBaseDados.addFornecedor(fornecedor1);
         gestorBaseDados.addSocio(socio3);
         gestorBaseDados.criarStockeLivros("Cinderela", "Joaquim", Genero.tecnico, SubGenero.informatica, 213, 345, 1222,3);
         gestorBaseDados.criarStockeLivros("O autocarro","Luis",Genero.ficcao,SubGenero.terror,333,476,2024,2);
